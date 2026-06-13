b7b54861a64637db72ba5e0f893bb3cde465d570
rimshad <rimshadpcs@gmail.com>
2026-06-13 12:29:30 +0100

    fix: disable unstable dev segment explorer

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  devIndicators: false,
+  experimental: {
+    devtoolSegmentExplorer: false,
+  },
+};
 
 export default nextConfig;
 
